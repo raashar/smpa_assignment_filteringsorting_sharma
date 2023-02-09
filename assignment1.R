a5ca44188eb16aa89bc7603cefda153664df736b
Riya Sharma <riya.sharma101@icloud.com>
2023-02-09 05:14:45 +0000

    First homework commit!

--- assignment1.R
+++ assignment1.R
@@ -24,7 +24,8 @@ View(housesales)
 # 1) The column "year" indicates the year. Filter to return only results from 2015. 
 # (note that because the column is numeric, you wouldn't use quotes as with text when setting your filter value)
 
-
+housesales %>%
+  filter(year == 2015)
 
 
 
